a85adaa1a65c01fcd2d09ff2281fbc6427157fae
04stefke <jovanovics399@yahoo.com>
2023-12-06 19:11:19 +0000

    fixed the uuid error, installed wrong dependency

--- todo-app/src/components/TodoWrapper.jsx
+++ todo-app/src/components/TodoWrapper.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
-import {v4 as uuidv4} from 'uuidv4'
+import {v4 as uuidv4} from 'uuid'
 import Todo from './Todo';
 import EditTodoForm from './EditTodoForm';
 uuidv4();
